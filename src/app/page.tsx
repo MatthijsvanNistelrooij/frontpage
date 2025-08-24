3db23c1214819e5341c5048365e5c39905e95ed0
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-24 02:05:48 +0200

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-28 md:gap-8">
             <ProjectCard
               url="breinoptimaal"
-              info="Een Brainy Webshop met veel kracht en een solide Checkout systeem voor Breinoptimaal."
+              info="Een Brainy Webshop met veel kracht en een solide Checkout voor Breinoptimaal."
               title="Breinoptimaal"
               image={breinoptimaal2}
               hoverImage={breinoptimaal}
