aeba0c99c7a5bdd6d89d9da840f9286e30ecdcaa
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-24 03:01:52 +0200

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <>
       <div className="relative w-full h-[60vh] lg:h-screen">
         <video
-          src="/stock_small.mp4"
+          src="/industir.mp4"
           autoPlay
           muted
           loop
@@ -25,7 +25,7 @@ export default function Home() {
         />
 
         <div className="absolute inset-0 flex items-center justify-center z-10">
-          <h1 className="text-white text-5xl px-8 md:text-8xl lg:text-[6.5rem] text-center drop-shadow-lg max-w-3xl lg:max-w-5xl">
+          <h1 className="text-white text-5xl px-8 md:text-8xl lg:text-[7rem] text-center drop-shadow-lg max-w-3xl lg:max-w-5xl mb-20">
             A spark of magic with every pixel.
           </h1>
         </div>
