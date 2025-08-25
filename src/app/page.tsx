cfda58eaf5e45738279b258c2e90c0e5a6afc249
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-25 02:11:52 +0200

    update marge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,11 +40,11 @@ export default function Home() {
             className="text-[#1a1a1a] text-xl md:text-2xl font-medium text-start max-w-md md:max-w-3xl tracking-wider my-20"
             style={{ fontFamily: "var(--font-mono)" }}
           >
-            Wij zijn een innovatief bedrijf dat vernieuwende techniek combineert
-            met design om projecten te realiseren die werken. Of het nu gaat om
-            een merk dat zich wil onderscheiden, een website die intuïtief is,
-            of een app die echt iets toevoegt – wij brengen de slimme,
-            doordachte aanpak. En dat doen we met liefde.😉
+            Wij zijn een innovatief bedrijf dat vernieuwende techniek en design
+            combineert om projecten te realiseren die echt werken. Of het nu
+            gaat om een merk dat zich wil onderscheiden, een website die
+            intuïtief is, of een app die iets toevoegt – wij leveren altijd een
+            slimme, doordachte aanpak. En dat doen we met liefde. 😉
           </div>
           <h2 className="text-6xl md:text-8xl font-light text-start mb-10 text-[#1a1a1a]">
             Ons Werk
